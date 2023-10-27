d500ddd3b7151bd32b5ca9649da205308c1a0d04
takashi58148 <toritu3anakin@gmail.com>
2023-10-27 18:26:27 +0900

    8-2コミットメント

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -5,10 +5,12 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Post; 
 
+
 class PostController extends Controller
 {
     public function index(Post $post)
     {
-        return $post->get();
+        return view('posts.index')->with(['posts' => $post->get()]);
     }
 }
+?>
