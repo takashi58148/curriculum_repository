f5b160f7de0c6c3f30ad9a60a48bd7d4736bbd11
takashi58148 <toritu3anakin@gmail.com>
2023-11-13 20:28:03 +0900

    一時的コミット

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,12 +4,14 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Post; 
+use App\Models\Category;
 
 
 class PostController extends Controller
 {
     public function index(Post $post)
     {
+        
         return view('posts.index')->with(['posts' => $post->getPaginateBylimit()]);
     }
     
@@ -17,5 +19,11 @@ class PostController extends Controller
     {
         return view('posts.show')->with(['post' => $post]);
     }
+    
+    public function create(Category $category)
+    {
+        return view('posts.create')->with(['categories' => $category->get()]);
+    }
+    
 }
 ?>
---
+++ resources/views/posts/edit.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE HTML>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <title>Blog</title>
+    </head>
+    <body>
+        <h1 class="title">編集画面</h1>
+        <div class="content">
+            <form action="/posts/{{ $post->id }}" method="POST">
+                @csrf
+                @method('PUT')
+                <div class='content__title'>
+                    <h2>タイトル</h2>
+                    <input type='text' name='post[title]' value="{{ $post->title }}">
+                    <p class='title__error' style="color:red">{{ $errors->first('post.title') }}</p>
+                </div>
+                <div class='content__body'>
+                    <h2>本文</h2>
+                    <textarea name="post[body]" placeholder="今日も一日お疲れさまでした。" >{{ $post->body }}</textarea>
+                    <p class="body__error" style="color:red">{{ $errors->first('post.body') }}</p>
+                </div>
+                <input type="submit" value="update">
+            </form>
+            <div class='footer'>
+                <a href="/posts/{{ $post->id }}">戻る</a>
+            </div>
+        </div>
+
+    </body>
+</html>
